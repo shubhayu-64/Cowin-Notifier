c16c7e08c30bf28fb084d82a9ec5a55a8c987431
shubhayu-64 <majumdarshubhayu@gmail.com>
2021-05-03 16:16:58 +0530

    Done and Dusted!

--- main.py
+++ main.py
@@ -2,31 +2,27 @@ import requests
 from datetime import date
 from datetime import datetime
 import json
+import time
 from pprint import pprint
+import tweepy
+import config
 
 
-def update_DateTime():
-    today = date.today()
-    day = today.strftime("%d-%m-%y")
-    print(day)
-    now = datetime.now()
-    time = now.strftime("%H:%M:%S")
-    print(time)
+def tweet(msg):
+    auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+    auth.set_access_token(config.access_token, config.access_token_secret)
+    api = tweepy.API(auth)
 
-    with open("data.json", "r") as data:
-        preData = json.load(data)
-    preData["date"] = day
-    preData["time"] = time
-    with open("data.json", "w") as data:
-        json.dump(preData, data)
+    api.update_status(msg)
 
 
-if __name__ == "__main__":
+def fetch_data():
     with open("districts.json", "r") as file:
         fetch = json.load(file)
-    today = date.today()
-    day = today.strftime("%d-%m-%y")
+    current_date = date.today()
+    day = current_date.strftime("%d-%m-%y")
     districts = fetch['districts']
+    c = 0
     for element in districts:
         district_id = element['district_id']
         link = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={district_id}&date={day}"
@@ -38,24 +34,19 @@ if __name__ == "__main__":
             name = centers['name']
             pincode = centers['pincode']
             fee = centers['fee_type']
+            state = centers['state_name']
             for sessions in centers['sessions']:
-                date = sessions['date']
+                cur_date = sessions['date']
                 min_age_limit = sessions['min_age_limit']
-                msg = f"Vaccine appointment available for: \n\n - Age: {min_age_limit}+ \n - On: {date}\n - Fee: {fee} \n\nIn {name}, {block_name}, {district_name}, {pincode} \n#COVID19 #COVID19Vaccine #CovidIndia #vaccination"
-                print(msg)
+                if sessions['available_capacity'] > 0:
+                    # print("Available")
+                    c = c + 1
+                    msg = f"Vaccine appointment available for: \n\n - Age: {min_age_limit}+ \n - On: {cur_date}\n - Fee: {fee} \n\nIn {name}, {block_name}, {district_name}, {state}, {pincode} \n#COVID19 #COVID19Vaccine #CovidIndia #vaccination #CowinNotifier #WestBengal"
+                    tweet(msg)
+                    time.sleep(3)
+        if c > 2:
+            break
 
-    '''
-    response = requests.get(
-        "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=725&date=03-05-2021")
-    data = response.json()
-    for centers in data['centers']:
-        block_name = centers['block_name']
-        district_name = centers['district_name']
-        name = centers['name']
-        pincode = centers['pincode']
-        fee = centers['fee_type']
-        for sessions in centers['sessions']:
-            date = sessions['date']
-            min_age_limit = sessions['min_age_limit']
-            msg = f"Vaccine appointment available for: \n\n - Age: {min_age_limit}+ \n - On: {date}\n - Fee: {fee} \n\nIn {name}, {block_name}, {district_name}, {pincode} \n#COVID19 #COVID19Vaccine #CovidIndia #vaccination"
-            print(msg)'''
+
+if __name__ == "__main__":
+    fetch_data()
